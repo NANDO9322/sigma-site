1233e0a2cb2a7c8924aa667db401605ed27d0d31
Henrique Souza <paulo18santos2000@gmail.com>
2022-11-17 18:51:50 -0300

    minha ultima atualização

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 //1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL
+function paghome(){
+  let items = document.querySelectorAll('.carousel .carousel-item')
+
+  items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+        // wrap carousel by using first child
+        next = items[0]
+      }
+      let cloneChild = next.cloneNode(true)
+      el.appendChild(cloneChild.children[0])
+      next = next.nextElementSibling
+    }
+  })
+}
 
 
 //2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS
