10e85ce1d8addc1da1dc58014c1f6cfc67cbe76c
henrique souza <paulo18santos2000@gmail.com>
2022-11-23 07:49:34 +0000

    identação

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 //1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL//  1_INICIAL
-function paghome(){
-  let items = document.querySelectorAll('.carousel .carousel-item')
+function paghome() {
+    let items = document.querySelectorAll('.carousel .carousel-item')
 
-  items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        // wrap carousel by using first child
-        next = items[0]
-      }
-      let cloneChild = next.cloneNode(true)
-      el.appendChild(cloneChild.children[0])
-      next = next.nextElementSibling
-    }
-  })
+    items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
 }
 
 
 //2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS//  2_PRODUTOS
-  
-  (function ($) {
+
+(function ($) {
     "use strict";
-    
+
     // Dropdown on mouse hover
     $(document).ready(function () {
         function toggleNavbarMethod() {
@@ -39,17 +39,20 @@ function paghome(){
         toggleNavbarMethod();
         $(window).resize(toggleNavbarMethod);
     });
-    
+
     // botao para voltar ao topo
-    $(document).ready(function(){
-      $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-circle-up' aria-hidden='true'></i></a>"), 
-      $(function toTop() {
-      $(window).scroll(function() {
-      $(this).scrollTop() > 400 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()}), $(".back-to-top").click(function() { 
-      return $("html, body").animate({
-      scrollTop: 0
-      }, 900), !1})});
-      })
+    $(document).ready(function () {
+        $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-circle-up' aria-hidden='true'></i></a>"),
+            $(function toTop() {
+                $(window).scroll(function () {
+                    $(this).scrollTop() > 400 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
+                }), $(".back-to-top").click(function () {
+                    return $("html, body").animate({
+                        scrollTop: 0
+                    }, 900), !1
+                })
+            });
+    })
 
     // Vendor carousel
     $('.vendor-carousel').owlCarousel({
@@ -59,20 +62,20 @@ function paghome(){
         autoplay: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-                items:2
+            0: {
+                items: 2
             },
-            576:{
-                items:3
+            576: {
+                items: 3
             },
-            768:{
-                items:4
+            768: {
+                items: 4
             },
-            992:{
-                items:5
+            992: {
+                items: 5
             },
-            1200:{
-                items:6
+            1200: {
+                items: 6
             }
         }
     });
@@ -86,17 +89,17 @@ function paghome(){
         autoplay: true,
         smartSpeed: 1000,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            576:{
-                items:2
+            576: {
+                items: 2
             },
-            768:{
-                items:3
+            768: {
+                items: 3
             },
-            992:{
-                items:4
+            992: {
+                items: 4
             }
         }
     });
@@ -104,20 +107,20 @@ function paghome(){
 
     // Product Quantity
     $('.quantity button').on('click', function () {
-      var button = $(this);
-      var oldValue = button.parent().parent().find('input').val();
-      if (button.hasClass('btn-plus')) {
-          var newVal = parseFloat(oldValue) + 1;
-      } else {
-          if (oldValue > 0) {
-              var newVal = parseFloat(oldValue) - 1;
-          } else {
-              newVal = 0;
-          }
-      }
-      button.parent().parent().find('input').val(newVal);
-  });
-  
+        var button = $(this);
+        var oldValue = button.parent().parent().find('input').val();
+        if (button.hasClass('btn-plus')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        button.parent().parent().find('input').val(newVal);
+    });
+
 })(jQuery);
 
 //3_CARRINHO//  3_CARRINHO//  3_CARRINHO//  3_CARRINHO//  3_CARRINHO//  3_CARRINHO//  3_CARRINHO
@@ -129,29 +132,29 @@ function paghome(){
 
 //  5_FINALIZAÇÃO//  5_FINALIZAÇÃO//  5_FINALIZAÇÃO//  5_FINALIZAÇÃO//  5_FINALIZAÇÃO//  5_FINALIZAÇÃO
 function openNav() {
-  document.getElementById("myNav").style.width = "20%";
+    document.getElementById("myNav").style.width = "20%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.width = "0%";
 }
 
-                            //funcão do cabeçalho para dispositivos com telas menores//
+//funcão do cabeçalho para dispositivos com telas menores//
 
-                            function openNav1() {
-                                document.getElementById("myNav1").style.width = "25%";
-                            }
-                            
-                            function closeNav1() {
-                                document.getElementById("myNav1").style.width = "0%";
-                            }
+function openNav1() {
+    document.getElementById("myNav1").style.width = "25%";
+}
+
+function closeNav1() {
+    document.getElementById("myNav1").style.width = "0%";
+}
 
 //  6_CADASTRO//  6_CADASTRO//  6_CADASTRO//  6_CADASTRO//  6_CADASTRO//  6_CADASTRO
-function comparar(){
-  if (document.getElementById("pf").checked){
-       window.location.href = "./pagina-01-cadastro-cpf.html";
-      }
-  else{
-      window.location.href = "./pagina-01-cadastro-cnpj.html";
-      }
-  }
+function comparar() {
+    if (document.getElementById("pf").checked) {
+        window.location.href = "./pagina-01-cadastro-cpf.html";
+    }
+    else {
+        window.location.href = "./pagina-01-cadastro-cnpj.html";
+    }
+}
