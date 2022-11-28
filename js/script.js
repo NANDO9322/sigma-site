006494a6841dafe68ca0749c5def2f3a8c2f29dd
henrique souza <paulo18santos2000@gmail.com>
2022-11-28 20:24:33 +0000

    Remoção do elemento back-to-top

--- js/script.js
+++ js/script.js
@@ -40,20 +40,6 @@ function paghome() {
         $(window).resize(toggleNavbarMethod);
     });
 
-    // botao para voltar ao topo
-    $(document).ready(function () {
-        $("body").append("<a href='#' class='back-to-top'><i class='fa fa-arrow-circle-up' aria-hidden='true'></i></a>"),
-            $(function toTop() {
-                $(window).scroll(function () {
-                    $(this).scrollTop() > 400 ? $(".back-to-top").fadeIn() : $(".back-to-top").fadeOut()
-                }), $(".back-to-top").click(function () {
-                    return $("html, body").animate({
-                        scrollTop: 0
-                    }, 900), !1
-                })
-            });
-    })
-
     // Vendor carousel
     $('.vendor-carousel').owlCarousel({
         loop: true,
